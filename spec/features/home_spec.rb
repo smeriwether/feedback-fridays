e1e913e35fd8570441f195ba3bf73a6a2fac3c58
Stephen Meriwether <stephen.meriwether@icloud.com>
2022-04-16 16:21:51 -0400

    Use lowercase test examples
     It's just a preference thing

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,19 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Navigates to the Home page', type: :feature do
-  it 'Sees feedback friday application title' do
+  it 'sees feedback friday application title' do
     visit '/'
 
     expect(page).to have_selector("[data-attr='title']")
   end
 
-  it 'Can add one teammate to the feedback session' do
+  it 'can add one teammate to the feedback session' do
     visit '/'
 
     expect(page).to have_selector("input[data-attr='teammate_1']")
   end
 
-  it 'Can add a second teammate to the feedback session' do
+  it 'can add a second teammate to the feedback session' do
     visit '/'
 
     expect(page).to have_selector("input[data-attr='teammate_2']")
