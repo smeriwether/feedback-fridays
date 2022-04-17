381b8813be5b039da16949f2295f4553eac36f6d
Stephen Meriwether <stephen.meriwether@icloud.com>
2022-04-17 16:01:07 -0400

    Add a link to start over in a top nav
    In case there's ever an issue and a user wants to start the whole thing
    over, they can now.

---
+++ spec/features/start_over_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Start over', type: :feature do
+  context 'when on the home page' do
+    it 'has no start over link' do
+      visit('/')
+      expect(page).not_to have_selector("[data-attr='start-over-link']")
+    end
+  end
+
+  context 'when on the lineup page' do
+    it 'has a start over link' do
+      session = FeedbackSession.create
+      visit new_feedback_lineup_path(session)
+      expect(page).to have_selector("[data-attr='start-over-link']")
+    end
+
+    it 'returns the user to the home page' do
+      session = FeedbackSession.create
+      visit new_feedback_lineup_path(session)
+      find("[data-attr='start-over-link']").click
+
+      expect(page).to have_current_path(root_path, ignore_query: true)
+    end
+  end
+
+  context 'when on the rotation page' do
+    it 'has a start over link' do
+      lineup = FeedbackLineup.create
+      visit feedback_rotation_path(lineup)
+      expect(page).to have_selector("[data-attr='start-over-link']")
+    end
+  end
+end
